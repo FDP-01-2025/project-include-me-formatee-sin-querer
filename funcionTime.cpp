fa04a2ae7a2257956ec9488f2ca0ad83ca92c7aa
Melara00180825 <00180825@uca.edu.sv>
2025-06-22 20:29:31 -0600

    commit de funcion del tiempo a medias

---
+++ funcionTime.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int temp(int seconds=5){
+
+    for (int i = seconds; i> 0; i-- ){
+        cout<<i<<" seconds" <<endl;
+     for (int j = 0; j> 10000000000;j++ ){
+        
+    }   
+    }
+    cout<<"time!"<<endl;
+}
+int main(){
+
+int seconds=6;
+
+temp(seconds);
+
+    return 0;
+}
