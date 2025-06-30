6e3b522d57cfa767d1122bcc7230147ab70876ea
Robert-Colcho <00054625@uca.edu.sv>
2025-06-30 16:50:58 -0600

    comentarios

--- pruebas/menu.h
+++ pruebas/menu.h
@@ -2,13 +2,14 @@
 #define MENU_H
 
 #include <iostream>
-#include "niveles.h"  // Incluimos el header con los niveles
+#include "niveles.h"  // Incluye funciones de los niveles
 using namespace std;
 
+// Función principal del menú del juego
 void menuGame() {
-    int option = 0, option2 = 0;
+    int option = 0, option2 = 0;  // Variables para controlar el menú y niveles
 
-    while (option != 2) {
+    while (option != 2) {  // Bucle principal hasta que el usuario decida salir
         cout << "-------- Menu ---------" << endl;
         cout << "1. Jugar" << endl;
         cout << "2. Salir" << endl;
@@ -18,9 +19,9 @@ void menuGame() {
         switch (option) {
         case 1:
             cout << "___________________________________" << endl;
-            option2 = 0;
-            while (option2 != 10) {
-                cout << "\n-------- Levels ---------" << endl;
+            option2 = 0;  // Reinicia la opción de nivel
+            while (option2 != 10) {  // Bucle para selección de niveles
+                cout << "\n-------- Niveles ---------" << endl;
                 cout << "1. Nivel 1" << endl;
                 cout << "2. Nivel 2" << endl;
                 cout << "3. Nivel 3" << endl;
@@ -35,7 +36,7 @@ void menuGame() {
                 cin >> option2;
 
                 if (option2 >= 1 && option2 <= 9) {
-                    playlevel(option2 - 1);
+                    playlevel(option2 - 1);  // Llama al nivel correspondiente (0-indexado)
                 } else if (option2 == 10) {
                     cout << "Regresando al menu principal..." << endl;
                 } else {
@@ -44,13 +45,13 @@ void menuGame() {
             }
             break;
         case 2:
-            cout << "Saliendo del programa" << endl;
+            cout << "Saliendo del programa" << endl;  // Finaliza el juego
             break;
         default:
-            cout << "Ingresa una opcion valida del menu" << endl;
+            cout << "Ingresa una opcion valida del menu" << endl;  // Opción no válida
             break;
         }
-        cout << endl;
+        cout << endl;   // Línea en blanco para separar iteraciones del menú
     }
 }
 
